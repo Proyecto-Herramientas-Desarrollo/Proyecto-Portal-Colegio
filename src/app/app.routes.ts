c3ac37317ae7cfedb17417debc9c5ff518ccc380
FBN-64 <falonso2024@gmail.com>
2026-05-11 16:46:20 -0500

    Cambios
    Agregados los cambios de las rutas
    No se agregó ningún código

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { MainContainer } from './dashboard/main-container/main-container';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: MainContainer },
+];
