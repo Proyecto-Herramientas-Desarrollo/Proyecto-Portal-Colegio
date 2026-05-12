636a8a01110d8bfefcafc9a21cb0fcd6983fc883
FBN-64 <falonso2024@gmail.com>
2026-05-12 12:23:14 -0500

    Dashboard
    - Se agregó una plantilla para el dashboard

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Importaciones de tus componentes
+import { PerfilMini } from '../shared/perfil-mini/perfil-mini';
+import { Cursos } from '../pages/alumno/cursos/cursos';
+import { Horario } from '../pages/alumno/horario/horario';
+import { Mensajes } from '../shared/mensajes/mensajes';
+import { Notificaciones } from '../shared/notificaciones/notificaciones';
+import { Finanzas } from '../pages/alumno/finanzas/finanzas';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    PerfilMini,
+    Cursos,
+    Horario,
+    Mensajes,
+    Notificaciones,
+    Finanzas,
+  ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class DashboardComponent {
+  seccionActiva: string = 'cursos';
+
+  cambiarSeccion(nuevaSeccion: string) {
+    this.seccionActiva = nuevaSeccion;
+  }
+}
