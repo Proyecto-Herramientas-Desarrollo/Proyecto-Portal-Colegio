55b3616cbaaeacbe67cea9f3d112457581b55e45
Diegormon <diegodavilaorihuela2107@gmail.com>
2026-05-12 21:17:12 -0500

    actualizacion del dashboard
    conexion con app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
+import { DashboardComponent } from './dashboard/dashboard'; // Importamos el trabajo de tu compañero
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
-
+  { path: 'dashboard', component: DashboardComponent }, // Esta es la ruta que faltaba
 ];
