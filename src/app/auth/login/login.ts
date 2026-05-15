e59d77315c97e9d016e62e4e12b48976eae30d87
Diegormon <diegodavilaorihuela2107@gmail.com>
2026-05-14 22:17:31 -0500

    actualizacion login

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-login',
-  imports: [], 
+  standalone: true,
+  imports: [FormsModule], 
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
-  
+  dni = '';
+  password = '';
+  mensajeError = '';
+
   constructor(private router: Router) {}
 
   
+  validarNumeros(event: Event) {
+    const input = event.target as HTMLInputElement;
+   
+    this.dni = input.value.replace(/[^0-9]/g, ''); 
+  }
+
+
   ingresar() {
-    this.router.navigate(['/dashboard']);
+    if (this.dni === '71235392' && this.password === '1234') {
+      this.mensajeError = ''; 
+      this.router.navigate(['/dashboard']); 
+    } else {
+     
+      this.mensajeError = 'DNI o contraseña incorrectos.';
+    }
   }
 }
