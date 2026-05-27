2847575f82d8ffc5f09bdfd64447aa133f5e6727
Diegormon <diegodavilaorihuela2107@gmail.com>
2026-05-26 23:29:18 -0500

    Feature: Implementada vista de cursos e info según aula_id

--- src/app/pages/alumno/cursos/cursos.ts
+++ src/app/pages/alumno/cursos/cursos.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SAN_ALFONSO_DB } from '../../../shared/data/db'; 
 
 @Component({
   selector: 'app-cursos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './cursos.html',
-  styleUrl: './cursos.css',
+  styleUrl: './cursos.css'
 })
-export class Cursos {}
+export class Cursos implements OnInit {
+  listaCursos: any[] = [];
+  alumnoActual: any;
+  aulaActual: any; // Variable para guardar el aula específica
+
+  ngOnInit() {
+    // 1. Obtenemos tus datos (ID 3)
+    this.alumnoActual = SAN_ALFONSO_DB.alumnos.find(a => a.id === 3);
+    
+    // 2. REQUISITO DEL TICKET: Información personalizada según su aula_id
+    if (this.alumnoActual) {
+      this.aulaActual = SAN_ALFONSO_DB.aulas.find(aula => aula.id === this.alumnoActual.aula_id);
+    }
+
+    // 3. Consumiendo las materias del catálogo
+    this.listaCursos = SAN_ALFONSO_DB.cursos;
+  }
+}
