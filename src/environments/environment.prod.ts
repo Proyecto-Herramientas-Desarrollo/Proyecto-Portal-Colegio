7e2f0622badecb747bdf01e8441f2997161e768b
FabrizeOo <fabrize19lion@gmail.com>
2026-06-28 23:21:28 -0500

    feat: agregar credenciales de Supabase y dependencia del cliente en environments y package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'TU_SUPABASE_URL',
-  supabaseKey: 'TU_SUPABASE_ANON_KEY',
+  supabaseUrl: 'https://nfuohhecfqyhicrbrgmb.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mdW9oaGVjZnF5aGljcmJyZ21iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2ODU1NzYsImV4cCI6MjA5ODI2MTU3Nn0.khuq3qMAwqDXGOmv0gAOevE7CjzzpA77C0Cn1RG5ILs',
 };
