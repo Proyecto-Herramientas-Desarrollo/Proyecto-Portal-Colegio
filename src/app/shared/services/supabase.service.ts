b1a267aff31ed7fed44cc2a7e7e13d03556f42b4
FBN-64 <falonso2024@gmail.com>
2026-06-30 11:31:20 -0500

    enviroment.prod
    - Se cambió el enviroment con enviroment.prod para que se quite un error

--- src/app/shared/services/supabase.service.ts
+++ src/app/shared/services/supabase.service.ts
@@ -1,6 +1,6 @@
+import { environment } from './../../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient, Session, User } from '@supabase/supabase-js';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
